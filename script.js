ede27899124d4ac38c94a82ce3b6c5bdb03ed5d8
KHallam <kjhallam321@gmail.com>
2020-10-11 17:19:14 -0500

    added 3 variables and 8 attributes to js file.

---
+++ script.js
@@ -0,0 +1,29 @@
+// Variables are Defined
+var  curTime = moment().format("LT");
+var  curTimeInt = parseInt(curTime);
+// console.log (curTime);
+var saveBtnEl = $(".saveBtn");
+
+//attributes set according to the hour/time
+$("Row-9").attr("data-time", moment("9:00 am", "h:mm:ss a").format("LT"));
+$("Row-10").attr("data-time", moment("10:00 am", "h:mm:ss a").format("LT"));
+$("Row-11").attr("data-time", moment("11:00 am", "h:mm:ss a").format("LT"));
+$("Row-12").attr("data-time", moment("12:00 pm", "h:mm:ss a").format("LT"));
+$("Row-1").attr("data-time", moment("1:00 pm", "h:mm:ss a").format("LT"));
+$("Row-2").attr("data-time", moment("2:00 pm", "h:mm:ss a").format("LT"));
+$("Row-3").attr("data-time", moment("3:00 pm", "h:mm:ss a").format("LT"));
+$("Row-4").attr("data-time", moment("4:00 pm", "h:mm:ss a").format("LT"));
+$("Row-5").attr("data-time", moment("5:00 pm", "h:mm:ss a").format("LT"));
+
+$(document).ready(function () {
+    renderSchedule();
+
+   $("#currentDay").text(currentDay); 
+   console.log(ready);
+})
+
+function renderSchedule () {
+    for(var i = 1; i <= 12; i++) {
+        $("Row" + i + "#").value(localStorage.getItem(i));
+    }
+}
