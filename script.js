43c0a7a2cd69c9394ec4b07228b7fc4a8a55e3eb
KHallam <kjhallam321@gmail.com>
2020-10-11 21:34:10 -0500

    debugging js script- still gliche on displaying time/date

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Variables are Defined
 var  curTime = moment().format("LT");
 var  curTimeInt = parseInt(curTime);
-// console.log (curTime);
 var saveBtnEl = $(".saveBtn");
 
 //attributes set according to the hour/time
@@ -16,22 +15,56 @@ $("Row-4").attr("data-time", moment("4:00 pm", "h:mm:ss a").format("LT"));
 $("Row-5").attr("data-time", moment("5:00 pm", "h:mm:ss a").format("LT"));
 
 $(document).ready(function () {
-    renderSchedule();
+        renderSchedule();
 
-    var currentDay = moment().format("MMM Do YYYY");
+    var currentDay = moment("#currentDay").format('MMMM Do YYYY, h:mm:ss a');
 
    $("#currentDay").text(currentDay); 
 
-   for (var t = 1; t <= 12; t++)
+   for (var t = 1; t <= 12; t++){
         var timeInput = $("Row" + t + "#").attr("data-time");
 
         var timeInputInt = parseInt(timeInput);
-        console.log(timeInputInt);
 
-})
+        if(curTimeInt === timeInputInt) {
+            $("Row" + t + "#").addClass("present");
+        }
 
-function renderSchedule () {
-    for(var i = 1; i <= 12; i++) {
-        $("Row" + i + "#").value(localStorage.getItem(i));
-    } 
-}
+        if (curTimeInt > timeInputInt) {
+            $("Row" + t + "#").addClass("past");
+        }
+
+        if (curTimeInt > timeInputInt) {
+            $("Row" + t + "#").addClass("future");
+        }
+    }
+
+    saveBtnEl.on("mouseenter", function (){
+        $(this).addClass("hover");
+        console.log("mouseenter")
+    });
+
+    saveBtnEl.on("mouseleave", function (){
+            $(this).removeClass("hover");
+            console.log("mouseleave")
+        });
+
+    saveBtnEl.on("click", function () {
+        var hourEl = $(this).attr("data-hour");
+
+        var agenda = $("Row" + hourEl + "#").value();
+
+        localStorage.setItem(hourEl, agenda);
+
+    });
+    
+    function renderSchedule () {
+        for(var i = 1; i <= 12; i++) {
+            $("Row" + i + "#").value(localStorage.getItem(i));
+        } 
+    }
+    
+
+    });
+
+
